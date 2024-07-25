a433a16323b064ced5a52f603129fab2fafe9eff
richel-k <richel.kembou@facsciences-uy1.cm>
2024-07-25 20:29:23 +0200

    envoi de test

---
+++ interface films avec js/data.js
@@ -0,0 +1,181 @@
+let movies = {
+    "RAYA AND THE LAST DRAGON" : {
+        name: "RAYA AND THE LAST DRAGON",
+        description : "Il y a de cela fort longtemps, au royaume imaginaire de Kumandra, humains et dragons vivaient en harmonie. Mais un jour, une force maléfique s’abattit sur le royaume et les dragons se sacrifièrent pour sauver l’humanité. Lorsque cette force réapparait cinq siècles plus tard, Raya, une guerrière solitaire, se met en quête du légendaire dernier dragon pour restaurer l’harmonie sur la terre de Kumandra, au sein d’un peuple désormais divisé. Commence pour elle un long voyage au cours duquel elle découvrira qu’il lui faudra bien plus qu’un dragon pour sauver le monde, et que la confiance et l’entraide seront essentiels pour conduire au succès cette périlleuse mission.",
+        background : "background/eran_raya.jpg"
+    },
+
+    "BLADE RUNNER 2049" : {
+        name: "BLADE RUNNER 2049",
+        description: "En 2049, la société est fragilisée par les nombreuses tensions entre les humains et leurs esclaves créés par bioingénierie. L’officier K est un Blade Runner : il fait partie d’une force d’intervention d’élite chargée de trouver et d’éliminer ceux qui n’obéissent pas aux ordres des humains. Lorsqu’il découvre un secret enfoui depuis longtemps et capable de changer le monde, les plus hautes instances décident que c’est à son tour d’être traqué et éliminé. Son seul espoir est de retrouver Rick Deckard, un ancien Blade Runner qui a disparu depuis des décennies",
+        background : "background/msg1688531080-7824.jpg"
+    },
+    "INCEPTION": {
+        name: "INCEPTION",
+        description : "Dom Cobb est un voleur expérimenté, le meilleur dans l'art dangereux de l'extraction, voler les secrets les plus intimes enfouis au plus profond du subconscient durant une phase de rêve, lorsque l'esprit est le plus vulnérable. Les capacités de Cobb ont fait des envieux dans le monde tourmenté de l'espionnage industriel alors qu'il devient fugitif en perdant tout ce qu'il a un jour aimé. Une chance de se racheter lui est alors offerte. Une ultime mission grâce à laquelle il pourrait retrouver sa vie passée mais uniquement s'il parvient à accomplir l'impossible inception.",
+        background : "background/Inception.jpg"
+
+    },
+    'HER': {
+        name : 'HER',
+        description : "Los Angeles, dans un futur proche. Theodore Twombly, un homme sensible au caractère complexe, est inconsolable suite à une rupture difficile. Il fait alors l'acquisition d'un programme informatique ultramoderne, capable de s'adapter à la personnalité de chaque utilisateur. En lançant le système, il fait la connaissance de 'Samantha', une voix féminine intelligente, intuitive et étonnamment drôle. Les besoins et les désirs de Samantha grandissent et évoluent, tout comme ceux de Theodore, et peu à peu, ils tombent amoureux…",
+        background : "background/Her.jpg"
+    },
+    'Gone Girl': {
+        name : 'Gone Girl', 
+        description : "Amy et Nick forment en apparence un couple modèle. Victimes de la crise, ils quittent Manhattan pour retourner s'installer dans la ville du Missouri où Nick a grandi. Mais le jour de leur 5ème anniversaire de mariage, Amy disparaît et Nick retrouve leur maison saccagée. Lors de l'enquête tout semble accuser Nick. Celui‐ci décide, de son côté, de tout faire pour savoir ce qui est arrivé à Amy et découvre qu'elle lui dissimulait beaucoup de choses.",
+        background : "background/Gone_girl.jpg"
+    },
+    "INTERSTELLAR": {
+        name : "INTERSTELLAR",
+        description : "Dans un futur proche, face à une Terre exsangue, un groupe d’explorateurs utilise un vaisseau interstellaire pour franchir un trou de ver permettant de parcourir des distances jusque‐là infranchissables. Leur but : trouver un nouveau foyer pour l’humanité.",
+        background : "background/Interstalar.jpg"
+    }, 
+    "DUNKIRK" : {
+        name : "DUNKIRK",
+        description : "Au début de la Seconde Guerre mondiale, en mai 1940, environ 400 000 soldats britanniques, français et belges se retrouvent encerclés par les troupes allemandes dans la poche de Dunkerque. L'Opération Dynamo est mise en place pour évacuer le Corps expéditionnaire britannique (CEB) vers l'Angleterre. L'histoire s'intéresse aux destins croisés des soldats, pilotes, marins et civils anglais durant l'Opération Dynamo. Alors que le CEB est évacué par le port et les plages de Dunkerque, trois soldats britanniques, avec un peu d'ingéniosité et de chance, arrivent à embarquer sous les bombardements.",
+        background : "background/Dunkirk.jpg"
+    }, 
+   "ATLAS":  {
+        name : "ATLAS",
+        description : "Quand la mission de capture d'un robot rebelle part à vau-l'eau, une brillante analyste spécialisée dans l’antiterrorisme se résout malgré elle à faire confiance à l'IA.",
+        background : "background/Atlas.jpg"
+    }, 
+    "JOY OF LIFE" : {
+        name : "JOY OF LIFE",
+        description : "Fan Xian est né dans l’ancien empire des Qing du Sud avec des souvenirs du 21ème siècle. Il vit dans une ville rurale en tant que fils illégitime du ministre des Finances, élevé par sa grand-mère. Sa mère, Ye Qing Mei, était autrefois une inventrice célèbre et la fondatrice du département Overwatch, le réseau d’espionnage du pays, mais a été tuée peu de temps après la naissance de son fils. Fan Xian a depuis été protégé par un expert en arts martiaux aveugle, Wu Zhu, le garde du corps de sa mère. Il a appris les arts martiaux et l’art du poison dans son enfance, également aidé par ses connaissances du 21e siècle. ",
+        background : "background/Joy_of_life.jpg"
+    },
+    "DIE HART 2" : {
+        name : "DIE HART 2",
+        description : "Dans cette suite, Kevin Hart, devenu la plus grande star mondiale de films d'action, devient la cible d'une vengeance diabolique. S'il veut survivre, il devra demander l'aide de sa partenaire à l'écran et d'un assistant un peu trop enthousiaste.",
+        background : "background/Die_hurt.jpg"
+    },
+    "SWORD SNOW STRIDE" : {
+        name: "SWORD SNOW STRIDE",
+        description : "Xu Xiao has risen to become King of Northern Liang at a time of great upheaval in China. Steppe kingdoms have risen with deadly intent, and rebellious uprisings threaten to destabilise the realm in the south. Xu Xiao has managed to defeat them all. His unconventional, free-spirited son and heir Xu Feng Nian has spent years on the road, honing his horse skills and fighting prowess. Along the way, he meets the double sword-wielding martial arts expert Nan Gong Pu She, who becomes his confidante. Also joining the royal household is a woman named Jiang Ni. At age 12, she joined the court of the Northern Liang. She is the princess of a rival kingdom, and could be harbouring deep sentiments of revenge.",
+        background : "background/snow_back.jpg"
+    }
+};
+let debut = 0;
+
+// commençons donc le Javascipt
+
+let images  = document.querySelectorAll(".scroll img");
+
+let verif = 0
+
+images.forEach(img => {
+    img.addEventListener("mouseover", function(){
+       // setTimeout(function()
+    //{
+        var title = document.getElementById("title")
+        
+        title.innerText = movies[img.alt].name
+
+        var bod = document.body;
+        console.log(movies[img.alt].background)
+        bod.style.backgroundImage ="linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6))," + `url('${movies[img.alt].background}')`
+
+    //}, 1)
+        
+
+    })
+})
+
+function to_add()
+{
+   var addpage = document.getElementById("add_films");
+   addpage.style.display = "flex"
+   
+}
+
+function get_new_file()
+{
+    
+    var title = document.getElementById("filee").files[0];
+    var des = 0
+    var tit = document.getElementById("titleadd")
+    //console.log("le titre est ", document.getElementById("filee").value);
+    
+    //console.log(movies)
+    let url = ""
+    
+    if(title)
+    {
+        const reader = new FileReader();
+
+        reader.onload = function(event)
+        {
+            const image_present = event.target.result;
+            const blob = new Blob([image_present]);
+            const urp = URL.createObjectURL(blob)
+            var img = document.createElement("img")
+            img.src = urp;
+            console.log("l'url est : ", urp)
+            img.alt = tit.value;
+            img.height = "300"
+            img.width = "300"
+            document.getElementById("scroll").appendChild(img)
+            document.getElementById("add_films").style.display = "none";
+            url = urp
+
+
+            var img_bac = document.getElementById("fond").files[0]
+
+            if(img_bac){
+                const read = new FileReader();
+                read.onload = function(event)
+                {
+                    const im_bac = event.target.result;
+                    const blo = new Blob([image_present]);
+                    const uri = URL.createObjectURL(blo)
+
+
+                    movies[tit.value] = {
+                        name : tit.value,
+                        description : document.getElementById("descriptionadd").value,
+                        background : uri,
+                        img : urp
+                    }
+                }
+
+                read.readAsArrayBuffer(img_bac)
+            }
+
+        }
+
+        reader.readAsArrayBuffer(title)
+    }
+    
+    
+
+    
+    /*const image_back = img_bac[0].result;
+    
+    const blo = new Blob([image_back]);
+    const ur = URL.createObjectURL(blo)*/
+
+
+    
+
+    console.log(movies[tit.value])
+    
+  
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
